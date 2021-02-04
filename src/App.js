417384d945bb6dcb24becf72daa84a9e77b6e845
sp-devi <spneuro01@gmail.com>
2021-02-04 23:42:27 +0900

    Add material-ui dependency

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './assets/mile-logo.jpeg';
+import { AppBar } from "@material-ui/core";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <AppBar>
+          <img src={logo} className="App-logo" alt="logo" />
+        </AppBar>
       </header>
     </div>
   );
