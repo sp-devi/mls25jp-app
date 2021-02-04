af41c2c1790e892e07e9d6a11d47fb5dcffca228
sp-devi <spneuro01@gmail.com>
2021-02-04 23:47:43 +0900

    Fix headers and add contents

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+const headers = [
+    {
+        label: "Home",
+        href: "/home"
+    },
+    {
+        label: "About Us",
+        href: "/missionStatement"
+    },
+    {
+        label: "Services",
+        href: "/services-offer"
+    }
+];
